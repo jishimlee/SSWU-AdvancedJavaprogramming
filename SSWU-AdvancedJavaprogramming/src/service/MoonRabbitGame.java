cb598996c55da06fba64136ed69d56215cfcdd88
ownue <enu_i@naver.com>
2024-11-09 11:24:01 +0900

    업로드 확인용

---
+++ SSWU-AdvancedJavaprogramming/src/service/MoonRabbitGame.java
@@ -0,0 +1,12 @@
+package service;
+
+import javax.swing.JFrame;
+
+public class MoonRabbitGame extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
