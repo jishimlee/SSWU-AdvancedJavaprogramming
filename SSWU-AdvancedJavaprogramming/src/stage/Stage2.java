d293985ad6f95a95ade41b61fb4f380a1fe9be60
heveidndksjd <kayoonkim@naver.com>
2024-11-22 15:01:22 +0900

    stage2 거북이 2마리

--- SSWU-AdvancedJavaprogramming/src/stage/Stage2.java
+++ SSWU-AdvancedJavaprogramming/src/stage/Stage2.java
@@ -16,7 +16,8 @@ public class Stage2 extends JPanel {
     private JLabel heartLabel;
     private JLabel timerLabel;
     private PlayerRabbit player;
-    private Turtle turtle;
+    private Turtle turtle1;
+    private Turtle turtle2;
     private Toad toad;
     private Tiger tiger;
 
@@ -39,7 +40,7 @@ public class Stage2 extends JPanel {
     private void initObject() {
         // 배경 이미지 설정
         this.frontMap = new JLabel(new ImageIcon("image/stage2.png"));
-        this.frontMap.setBounds(0, 0, 1000, 640); // 배경 이미지 크기 설정, 겹치는거 아닌가..?
+        this.frontMap.setBounds(0, 0, 1000, 640); // 배경 이미지 크기 설정
         this.setLayout(null); 
         this.add(this.frontMap); 
         this.setVisible(true);
@@ -72,12 +73,15 @@ public class Stage2 extends JPanel {
     private void initThread() {
         SwingUtilities.invokeLater(() -> {
             // Stage1 초기화가 완료된 후에 Turtle 생성
-            this.turtle = new Turtle(100, 255, false, this.game, this.player);
-            this.toad = new Toad(750, 250, false, this.game, this.player);
-            this.frontMap.add(this.turtle);
-            this.frontMap.add(this.toad);
-            new Thread(() -> turtle.start()).start(); // Turtle 실행
-            new Thread(() -> toad.start()).start();
+            this.turtle1 = new Turtle(750, 145, false, this.game, this.player);
+            this.turtle2 = new Turtle(200, 464, false, this.game, this.player);
+            //this.toad = new Toad(750, 250, false, this.game, this.player);
+            this.frontMap.add(this.turtle1);
+            this.frontMap.add(this.turtle2);
+            //this.frontMap.add(this.toad);
+            new Thread(() -> turtle1.start()).start(); // Turtle 실행
+            new Thread(() -> turtle2.start()).start();
+            //new Thread(() -> toad.start()).start();
         });
     }
     
