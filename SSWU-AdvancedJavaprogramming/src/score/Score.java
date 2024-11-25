ed5030393cfd6efd9437cc383031811f6504f825
JiminLee <143867922+jishimlee@users.noreply.github.com>
2024-11-25 23:59:51 +0900

    Update Score.java

--- SSWU-AdvancedJavaprogramming/src/score/Score.java
+++ SSWU-AdvancedJavaprogramming/src/score/Score.java
@@ -1,1 +1,24 @@
+package score;
+
+public class Score {
+	private int score;
+	
+	public Score() {
+		score = 0;
+	}
+	public int getScore() {
+		// 점수를 얻는 함수 
+		return score;
+	}
+	
+	public void addScore(int points) {
+		// 점수를 더하는 함수 
+        score += points;
+        //System.out.println("점수: " + score);
+    }
+	
+	public void resetScore() {
+		score = 0;
+	}
+}
 
