afec8960ef96bfc2f91f477c7f543e066e4acd01
JiminLee <143867922+jishimlee@users.noreply.github.com>
2024-12-01 02:56:43 +0900

    Update Score.java

--- SSWU-AdvancedJavaprogramming/src/score/Score.java
+++ SSWU-AdvancedJavaprogramming/src/score/Score.java
@@ -1,24 +1,65 @@
 package score;
-import javax.swing.JLabel;
+
+import javax.swing.JPanel;
+
+import main.MoonRabbitGame;
+import stage.*;
 
 public class Score {
-	private int score;
-	
-	public Score() {
-		this.score = 0;
-	}
-	public int getScore() {
-		return score;
-	}
-	
-	public void addScore(int points) {
-		// 점수를 더하는 함수 
-        score += points;
-        System.out.println("점수: " + score);
+    private static int score;
+    private MoonRabbitGame game;
+    public boolean scoreUp = false;
+    public Stage1 stage1;
+    public Stage2 stage2;
+    public Stage3 stage3;
+    public Stage4 stage4;
+    public Stage5 stage5;
+    public JPanel stage;
+    public int stageNum;
+    
+    public Score(MoonRabbitGame game) {
+        score = 0;
+        this.game = game;
+        this.stage = game.getCurrentStage();
+        this.stageNum = game.getStageNumber();
     }
-	
-	public void resetScore() {
-		this.score = 0;
-	}
-}
 
+    public int getCurrentScore() {
+        return this.score;
+    }
+
+    public void setStage(JPanel stage) {
+        this.stage = stage;
+    }
+
+    public void addScore(int points) {
+        this.score += points; // 점수 추가
+        this.scoreUp = true;  // 점수 업데이트 플래그 설정
+        System.out.println("Score updated: " + this.score + ", scoreUp 상태: " + this.scoreUp);
+        if (stage instanceof Stage1) {
+            ((Stage1) stage).updateScore(); // Stage4의 loadLifeIcon 호출
+        }
+        if (stage instanceof Stage2) {
+            ((Stage2) stage).updateScore(); // Stage4의 loadLifeIcon 호출
+        }
+        if (stage instanceof Stage3) {
+            ((Stage3) stage).updateScore(); // Stage4의 loadLifeIcon 호출
+        }
+
+        if (stage instanceof Stage4) {
+            ((Stage4) stage).updateScore(); // Stage4의 loadLifeIcon 호출
+        }
+
+        if (stage instanceof Stage5) {
+            ((Stage5) stage).updateScore(); // Stage4의 loadLifeIcon 호출
+        }
+
+    }
+
+
+
+    public void resetScore() {
+        //this.score = 0;
+        this.scoreUp = false;
+    }
+}
